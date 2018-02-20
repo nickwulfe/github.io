62c211776c06611ab851f70711a90b42b98808d3
Joshua <nickwulfe@gmail.com>
2018-02-20 18:41:51 -0500

    added js file and code

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
-Math.random(100);
+button.onclick = function random() {
+    button.addEventListener("click", random);
+    var random = Math.floor(Math.random() * 100);
+    console.log(random);
+};
