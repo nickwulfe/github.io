691df62a7525f824a7cafe4e526609ecfb2cbf98
Joshua <nickwulfe@gmail.com>
2018-02-20 19:37:42 -0500

    adjusted location of comments for script

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
+//this is the random D100 with an alert box to tell the visitor how lucky they are//
+
 document.getElementById('random').onclick = function() {
     var random = Math.floor(Math.random() * 100);
     alert('You are ' + random + '% lucky today!');
 }
-
-//this is the random D100 with an alert box to tell the visitor how lucky they are//
